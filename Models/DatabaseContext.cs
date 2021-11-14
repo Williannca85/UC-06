542967451479f94ede92e56847858b76b5635a79
Williannca85 <williannca@hotmail.com>
2021-11-13 22:14:31 -0300

    primeiro commit

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Hotsite.Models
+{
+    public class DatabaseContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;DataBase=Hotsite;Uid=root;Pwd=;");
+        }
+
+        public DbSet<Interesse> Interesses { get; set; }
+
+    }
+}
